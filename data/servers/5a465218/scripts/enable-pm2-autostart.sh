5d4383eb7939df3b30ed6f76d51a1fe8cc73ac99
novanet 🧑‍🔧 <novanet403@gmail.com>
2026-08-19 18:34:23 +0700

    Delete data directory

--- data/servers/5a465218/scripts/enable-pm2-autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
-
-pm2 start ecosystem.config.js
-pm2 save
-echo
- echo "PM2 startup command berikut akan dicetak. Jalankan perintah sudo yang diberikan PM2, lalu server akan menghidupkan Telegram SaaS otomatis setiap boot."
-pm2 startup
--- data/servers/86c35694/.novanet-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export HOME=/root
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export NODE_OPTIONS="--max-old-space-size=512"
-
-cd /home/container
-
-exec node index.js
