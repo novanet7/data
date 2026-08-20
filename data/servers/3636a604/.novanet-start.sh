27f27019e25d6e7ebce1f7ad85056732635ea3e7
novanet 🧑‍🔧 <novanet403@gmail.com>
2026-08-20 10:00:02 +0700

    Backup: tambah data/servers/3636a604/.novanet-start.sh — 2026-08-20T03:00:02.187Z

---
+++ data/servers/3636a604/.novanet-start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export HOME=/root
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export NODE_OPTIONS="--max-old-space-size=512"
+
+cd /home/container
+
+exec npm start
